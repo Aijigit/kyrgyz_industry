2e1bf827cfffcb7ab09388e1bb7fdfe81cb2bfbb
Aijigit Nabiev <ajzigit24@gmail.com>
2022-10-11 14:11:13 +0600

    Инициализация проекта 5

--- app_blog/main/urls.py
+++ app_blog/main/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('detail/', views.detail, name='detail'),
+    path('admin-panel/', views.admin_index_page, name='admin_panel'),
+    path('admin-panel/add/', views.admin_form_page, name='forms_page')
 ]
--- app_blog/main/views.py
+++ app_blog/main/views.py
@@ -4,10 +4,18 @@ from django.shortcuts import render
 
 
 def index(request):
-    template_name = "index.html"
+    template_name = "client/index.html"
     return render(request, template_name, {})
 
 
 def detail(request):
-    template_name = "blog_detail.html"
+    template_name = "client/pages/blog_detail.html"
+    return render(request, template_name, {})
+
+def admin_index_page(request):
+    template_name = "admin/admin.html"
+    return render(request, template_name, {})
+
+def admin_form_page(request):
+    template_name = "admin/pages/forms/basic_elements.html"
     return render(request, template_name, {})
